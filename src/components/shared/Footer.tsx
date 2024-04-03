e44af569f9707e8c155d05415812abbb294c0702
shukumar-542 <shukumar542@gmail.com>
2024-04-03 14:56:25 +0600

    Enhancment footer section

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,33 +1,37 @@
 import Link from 'next/link';
 import React from 'react';
+import beforeImg from "@/assets/before.webp"
+import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <footer className="footer relative bg-[#FFD835] footer-center p-10  text-base-content rounded">
             <nav >
-
-                <ul className="grid grid-cols-1 md:grid-cols-5 gap-4 text-xl font-serif">
-                    <li className='text-[#2C188D]'>
+                <div className=" absolute top-[-40px]">
+                    <Image src={beforeImg} className="w-full h-[100px]" height={400} width={1600} alt="testimonials" />
+                </div>
+                <ul className="grid grid-cols-1 md:grid-cols-5 gap-4 mt-10 text-xl font-serif">
+                    <li className='text-[#4B0800]'>
                         <Link href="/popular-products">Trending Products</Link>
                     </li>
-                    <li className='text-[#FD6A02]'>
+                    <li className='text-[#4B0800]'>
                         <Link href="/category">Category</Link>
                     </li>
-                    <li className='text-[#2C188D]'>
+                    <li className='text-[#4B0800]'>
                         <Link href="/about-us">About Us</Link>
                     </li>
-                    <li className='text-[#FD6A02]'>
+                    <li className='text-[#4B0800]'>
                         <Link href="/contact-us">Contact Us</Link>
                     </li>
-                    <li className='text-[#2C188D]'>
+                    <li className='text-[#4B0800]'>
                         <Link href="/customer-care">Customer Care</Link>
                     </li>
                 </ul>
             </nav>
-            <div className='grid grid-cols-2 md:grid-cols-5 w-[100%]'>
+            <div className='grid grid-cols-2 md:grid-cols-5 w-[100%] border-b-2 border-dashed pb-8 border-[#F1C200]'>
                 <div >
                     <ul className='text-start'>
-                        <li className="font-bold">Home</li>
+                        <li className="font-bold  text-[#4B0800]">Home</li>
                         <li>Pricing</li>
                         <li>Overview</li>
                         <li>Browse</li>
@@ -35,7 +39,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <ul className='text-start'>
+                    <ul className='text-start text-[#4B0800]'>
                         <li className="font-bold">About</li>
                         <li>Pricing</li>
                         <li>Overview</li>
@@ -45,7 +49,7 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul className='text-start'>
-                        <li className="font-bold">Contact Us</li>
+                        <li className="font-bold text-[#4B0800]">Contact Us</li>
                         <li>Pricing</li>
                         <li>Overview</li>
                         <li>Browse</li>
@@ -54,7 +58,7 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul className='text-start'>
-                        <li className="font-bold">Products</li>
+                        <li className="font-bold text-[#4B0800]">Products</li>
                         <li>Milk</li>
                         <li>Cerials</li>
                         <li>Cookies</li>
@@ -63,7 +67,7 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul className='text-start'>
-                        <li className="font-bold">Legals Page</li>
+                        <li className="font-bold text-[#4B0800]">Legals Page</li>
                         <li>Dahshboard</li>
                         <li>Cerials</li>
                         <li>Cookies</li>
@@ -71,16 +75,19 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <a className='cursor-pointer text-sky-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-                    <a className='cursor-pointer text-red-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-                    <a className='cursor-pointer text-sky-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-                </div>
-            </nav>
-            <aside>
-                <p>Copyright © 2024 - All right reserved by Baby Food Ltd</p>
-            </aside>
+            <div className='grid grid-cols-1 md:grid-cols-2 justify-between'>
+               
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Baby Food Ltd</p>
+                </aside>
+                <nav>
+                    <div className="grid grid-flow-col gap-4">
+                        <a className='cursor-pointer text-sky-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+                        <a className='cursor-pointer text-red-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+                        <a className='cursor-pointer text-sky-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+                    </div>
+                </nav>
+            </div>
         </footer>
     );
 };
--- src/components/ui/Testimonials.tsx
+++ src/components/ui/Testimonials.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
-import styles from "../shared/Banner.module.css";
-import text1 from '@/assets/testimonial_2.webp'
-import text2 from '@/assets/before.webp'
+import tesimonials1 from '@/assets/testimonial_1.webp'
+import tesimonials2 from '@/assets/testimonial_2.webp'
+import tesimonials3 from '@/assets/testimonial_3.webp'
+import before from '@/assets/before.webp'
+
 const Testimonials = () => {
     return (
-        <div className={`testimonials relative py-10 my-20`}>
+        <div className={`testimonials relative py-10 my-20 `}>
             <div className=" absolute top-[-40px]">
-                <Image src={text2}  className="w-full h-[100px]" height={400} width={1000} alt="testimonials" />
+                <Image src={before} className="w-full h-[100px]" height={400} width={1600} alt="testimonials" />
             </div>
             <div className="text-center my-20 ">
-                <h6>TESTIMONIAL</h6>
+                <h6 className="text-[#E16525]">TESTIMONIAL</h6>
                 <h2 className="text-3xl font-bold text-[#4B0800]">What Our Customers Say</h2>
-                </div>
+            </div>
             <div className="container">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-2">
                     <div className="relative">
                         <div className=" absolute top-[-15%] left-[30%] flex items-center justify-center ">
-                            <Image src={text1} height={100} width={140} alt="testimonials" />
+                            <Image src={tesimonials1} height={100} width={140} alt="testimonials" />
                         </div>
+                       
                         <div className="bg-white mt-10 rounded-md my-10 p-5 text-center">
 
                             <div className="mt-14">
@@ -28,7 +31,7 @@ const Testimonials = () => {
                     </div>
                     <div className="relative">
                         <div className=" absolute top-[-15%] left-[30%] flex items-center justify-center ">
-                            <Image src={text1} height={100} width={140} alt="testimonials" />
+                            <Image src={tesimonials2} height={100} width={140} alt="testimonials" />
                         </div>
                         <div className="bg-white mt-10 rounded-md my-10 p-5 text-center">
 
@@ -40,7 +43,7 @@ const Testimonials = () => {
                     </div>
                     <div className="relative">
                         <div className=" absolute top-[-15%] left-[30%] flex items-center justify-center ">
-                            <Image src={text1} height={100} width={140} alt="testimonials" />
+                            <Image src={tesimonials3} height={100} width={140} alt="testimonials" />
                         </div>
                         <div className="bg-white mt-10 rounded-md my-10 p-5 text-center">
 
