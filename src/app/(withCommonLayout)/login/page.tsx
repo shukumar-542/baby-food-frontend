b72eeb9d1deade126dd43966242df7e266183841
shukumar-542 <shukumar542@gmail.com>
2024-04-18 23:43:20 +0600

    Implement react hook for login and register page

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -1,9 +1,26 @@
+"use client"
 import Link from 'next/link';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { CiUnlock } from 'react-icons/ci';
 import { MdEmail } from 'react-icons/md';
 
+
+type Inputs = {
+    email: string
+    password: string
+  }
 const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<Inputs>()
+
+
+      const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log(data)
+    }
     return (
         <div className='container'>
             <div className='flex  items-center  h-[100vh] my-auto'>
@@ -15,23 +32,24 @@ const LoginPage = () => {
 
 
                     <div className='mt-5'>
-                        <div className="flex items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
-                            <MdEmail className="text-gray-500 mr-2 w-6  size-6" />
-                            <input type="email" placeholder="Email" className="flex-grow outline-none border-none" />
-                        </div>
-                        <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
-                            <CiUnlock className="text-gray-500 mr-2 w-6  size-6" />
-                            <input type="email" placeholder="Email" className="flex-grow outline-none border-none" />
-                        </div>
-                        <div className='flex items-center mt-3'>
-                            <MdEmail className="text-gray-500 mr-2 w-6  " />
-                            <p className='text-sm'>Forgot your <span className='text-blue-500'>Password?</span></p>
-                        </div>
-                        <div>
-                            <p className='text-sm mt-3 ml-1'>If you have an account, please <Link href='/register'><span className='text-blue-500 '> Register Here</span></Link></p>
-                        </div>
-                        <button className="mt-5 hover:bg-[#D3710F] px-5 py-2 bg-black text-white rounded-md">LOGIN</button>
-
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="flex items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
+                                <MdEmail className="text-gray-500 mr-2 w-6  size-6" />
+                                <input type="email" placeholder="Email" className="flex-grow outline-none border-none" {...register("email")} />
+                            </div>
+                            <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
+                                <CiUnlock className="text-gray-500 mr-2 w-6  size-6" />
+                                <input type="password" placeholder="password" className="flex-grow outline-none border-none" {...register("password")} />
+                            </div>
+                            <div className='flex items-center mt-3'>
+                                <MdEmail className="text-gray-500 mr-2 w-6  " />
+                                <p className='text-sm'>Forgot your <span className='text-blue-500'>Password?</span></p>
+                            </div>
+                            <div>
+                                <p className='text-sm mt-3 ml-1'>If you have an account, please <Link href='/register'><span className='text-blue-500 '> Register Here</span></Link></p>
+                            </div>
+                            <button type='submit' className="mt-5 hover:bg-[#D3710F] px-5 py-2 bg-black text-white rounded-md">LOGIN</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/app/(withCommonLayout)/register/page.tsx
+++ src/app/(withCommonLayout)/register/page.tsx
@@ -1,10 +1,29 @@
+"use client"
 import Link from 'next/link';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { BiUser } from 'react-icons/bi';
 import { CiUnlock } from 'react-icons/ci';
 import { MdEmail } from 'react-icons/md';
 
+type Inputs = {
+    name: string
+    email: string
+    password: string
+}
+
 const RegisterPage = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>()
+
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log(data)
+    }
     return (
         <div className='container'>
             <div className='flex  items-center  h-[100vh] my-auto'>
@@ -15,26 +34,29 @@ const RegisterPage = () => {
 
 
                     <div className='mt-5'>
-                        <div className="flex items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
-                            <BiUser className="text-gray-500 mr-2 w-6  size-6" />
-                            <input type="email" placeholder="User Name" className="flex-grow outline-none border-none" />
-                        </div>
-                        <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
-                            <MdEmail className="text-gray-500 mr-2 w-6  size-6" />
-                            <input type="email" placeholder="Email" className="flex-grow outline-none border-none" />
-                        </div>
-                        <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
-                            <CiUnlock className="text-gray-500 mr-2 w-6  size-6" />
-                            <input type="password" placeholder="password" className="flex-grow outline-none border-none" />
-                        </div>
-                        <div className='flex items-center mt-3'>
-                            <MdEmail className="text-gray-500 mr-2 w-6  " />
-                            <p className='text-sm'>Forgot your <span className='text-blue-500'>Password?</span></p>
-                        </div>
-                        <div>
-                            <p className='text-sm mt-3 ml-1'>If you have an account, please <Link href='/login'><span className='text-blue-500 '> Login Here</span></Link></p>
-                        </div>
-                        <button className="mt-5 hover:bg-[#D3710F] px-5 py-2 bg-black text-white rounded-md">LOGIN</button>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+
+                            <div className="flex items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
+                                <BiUser className="text-gray-500 mr-2 w-6  size-6" />
+                                <input type="name" placeholder="User Name" className="flex-grow outline-none border-none" {...register("name")} />
+                            </div>
+                            <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
+                                <MdEmail className="text-gray-500 mr-2 w-6  size-6" />
+                                <input type="email" placeholder="Email" className="flex-grow outline-none border-none" {...register("email")} />
+                            </div>
+                            <div className="flex mt-5 items-center gap-2 border hover:border-red-400 border-gray-300 rounded-lg p-2">
+                                <CiUnlock className="text-gray-500 mr-2 w-6  size-6" />
+                                <input type="password" placeholder="password" className="flex-grow outline-none border-none" {...register("password")} />
+                            </div>
+                            <div className='flex items-center mt-3'>
+                                <MdEmail className="text-gray-500 mr-2 w-6  " />
+                                <p className='text-sm'>Forgot your <span className='text-blue-500'>Password?</span></p>
+                            </div>
+                            <div>
+                                <p className='text-sm mt-3 ml-1'>If you have an account, please <Link href='/login'><span className='text-blue-500 '> Login Here</span></Link></p>
+                            </div>
+                            <button type='submit' className="mt-5 hover:bg-[#D3710F] px-5 py-2 bg-black text-white rounded-md">Register</button>
+                        </form>
 
                     </div>
                 </div>
