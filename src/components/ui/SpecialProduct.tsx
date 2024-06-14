a0314f4439b4c9bbd5d83fe5b4e2b50bad026b89
shukumar-542 <shukumar542@gmail.com>
2024-06-14 19:09:34 +0600

    Enhacement special product section responsive

--- src/components/ui/SpecialProduct.tsx
+++ src/components/ui/SpecialProduct.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 import straw from "@/assets/p11-318x318.jpg"
 const SpecialProduct = () => {
     return (
-        <div className={`${styles.special_product} mb-14`}>
+        <div className={`${styles.special_product} mb-14 py-5`}>
             <div className='py-10 text-center'>
                 <h1 className='text-xl text-white'>FRESH ORGANIC FOODS DELIVERY MADE EASY
                 </h1>
                 <p className='text-3xl text-white font-bold'>SPECIAL PRODUCT</p>
             </div>
-            <div className=' relative py-10 container flex text-white justify-between gap-10'>
+            <div className=' relative py-10 container grid grid-cols-1 md:grid-cols-2 text-white  gap-10'>
                 <div className='space-y-5'>
                     <div className='bg-[#685C88] rounded-lg p-10'>
                         <div className='w-[70%] '>
@@ -40,7 +40,7 @@ const SpecialProduct = () => {
                         </div>
                     </div>
                 </div>
-                <div className='absolute top-[-0%] left-[32%] '>
+                <div className='absolute hidden md:block  top-[5%] left-[32%] '>
                     <Image src={straw} height={300} width={300} className='bg-white shadow-2xl rounded-full size-96' alt='strabarry' />
                 </div>
             </div>
