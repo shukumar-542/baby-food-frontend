7f88d5badaf0f279f829a0c981c1740e7b419433
shukumar-542 <shukumar542@gmail.com>
2024-06-20 23:46:40 +0600

    create a loding component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const loading = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <h1 className='text-2xl font-bold text-[#FD6A02]'>Loading..</h1>
+            {/* <h1 className='text-2xl font-bold text-[#FD6A02]'>Loading..</h1> */}
+            <span className="loading loading-dots loading-lg text-[#FD6A02]"></span>
         </div>
     );
 };
