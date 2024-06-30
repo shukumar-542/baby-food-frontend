42f4954249b8a6300922bcf019470af9040a25da
shukumar-542 <shukumar542@gmail.com>
2024-06-30 21:12:52 +0600

    Implement show user name role and image on the nav bar

--- src/components/ui/UserNavbar/UserNavbar.tsx
+++ src/components/ui/UserNavbar/UserNavbar.tsx
@@ -1,4 +1,6 @@
+import { useGetUserInformationQuery } from '@/redux/api/userApi';
 import { getUserInfo } from '@/services/auth.service';
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { FaUser } from 'react-icons/fa';
@@ -13,19 +15,35 @@ interface UserInfo {
 const UserNavbar = () => {
 
     const [user, setUserRole] = useState<UserInfo | null>();
+
+
+    // Get user email and phone number by using email
+    const { data: userInfo, error, isLoading } = useGetUserInformationQuery(user?.email);
+    // console.log(userInfo);
     useEffect(() => {
         const user = getUserInfo()
         setUserRole(user)
     }, [])
     return (
-        <div className="shadow-md bg-gray-200 h-14 flex items-center justify-end text-end gap-5 pr-16 sticky top-0 z-10">
+        <div className="shadow-md bg-gray-200 h-14 flex items-center justify-end text-end gap-3 pr-16 sticky top-0 z-10">
             <Link href={'/dashboard/update-profile'}>
-                <div className='cursor-pointer  '>
-                    <p className="font-semibold">{user?.email} </p>
-                    <p>{user?.role} </p>
+                <div className='cursor-pointer hover:text-[#01B0B9]  '>
+                    <p className="font-semibold ">{userInfo?.name} </p>
+                    <p className='text-sm'>{user?.role} </p>
                 </div>
             </Link>
-            <FaUser size={25} />
+
+            {
+                userInfo?.image ? <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <Image
+                            height={50} width={50}
+                            alt="Image"
+                            src={userInfo?.image} />
+                    </div>
+                </div> : <FaUser size={25} />
+            }
+
 
         </div>
     );
